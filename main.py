44b96b49469cf7a91e4ebcfbda532e78ca11f875
yasmin-alt-3 <yasmin.msec@gmail.com>
2025-11-27 10:17:54 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import webapp2
+class Temp(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write('''
+<h2>Celcius to Farenheit</h2>
+<form method="post">
+    Enter temp: <input name="c">
+    <input type="submit" value="Convert">
+</form>
+''')
+
+    def post(self):
+        c=float(self.request.get('c'))
+        f=(c*9/5)+32
+        self.response.out.write('<h3>Farenheit = %.2f</h3>'%f)
+        self.response.out.write('<a href="/">Back</a>')
+app=webapp2.WSGApplication([('/',Temp)],debug=True)
